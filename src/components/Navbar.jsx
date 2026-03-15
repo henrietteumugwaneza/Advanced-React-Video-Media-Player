93ac29ee626596102d85ce78e43638bf4b466294
henrietteumugwaneza <umugwanezahenriette13@gmail.com>
2026-03-15 15:59:11 +0200

    top navigation

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from "react";  
+import SearchBar from "./SearchBar";
+import "../styles/navbar.css";
+
+function Navbar(){
+
+return(
+
+<header className="navbar">
+
+<div className="logo">
+YouTubeClone
+</div>
+
+<SearchBar/>
+
+</header>
+
+)
+
+}
+
+export default Navbar 
