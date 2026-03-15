a9c7f566efeff6bbfc7e8d817285a705e627222e
henrietteumugwaneza <umugwanezahenriette13@gmail.com>
2026-03-15 16:11:45 +0200

    videoDetail page

---
+++ src/pages/VideoDetails.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useParams } from "react-router-dom"
+import { useQuery } from "@tanstack/react-query"
+import { useParams } from "react-router-dom"
+import { useQuery } from "@tanstack/react-query"
+
+import { fetchFromAPI } from "../api/fetchFromAPI"
+import VideoPlayer from "../components/VideoPlayer"
+import Loader from "../components/Loader"
+
+function VideoDetails(){
+
+const {id} = useParams()
+
+const {data,isLoading} = useQuery({
+
+queryKey:["video",id],
+
+queryFn:()=>fetchFromAPI(`/videos?part=snippet,statistics&id=${id}`)
+
+})
+
+if(isLoading) return <Loader/>
+
+const video = data.items[0]
+
+return(
+
+<div>
+
+<VideoPlayer videoId={id}/>
+
+<h2>{video.snippet.title}</h2>
+
+<p>{video.statistics.viewCount} views</p>
+
+<p>{video.snippet.description}</p>
+
+</div>
+
+)
+
+}
+
+export default VideoDetails
